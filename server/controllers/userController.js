f3cfd7a1cb7a5bb1ba57e9e88cbf99d943f709a8
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2023-11-09 15:41:53 +0100

    Changes to User model (id)

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,112 +1,103 @@
 const { Users } = require("../models/User");
-const {verify}=require('jsonwebtoken')
-
-
-
+const { verify } = require("jsonwebtoken");
+const { v4 } = require("uuid");
 
 //login of user
 async function CheckSignInController(req, res, next) {
-  const { email, password } = req.body;
-  try {
-    const finduser = await Users.findOne({ email });
-    // if (!finduser) {
-    //   next({ status: 404, message: "user not found you need to signup" });
-    //   return;
-    // }
-    if (finduser.password !== password) {
-        const err=new Error('Wrong password !')
-        err.status=401
-      next(err);
-      return;
-    }
-    const last_login=new Date()
-    const updateuser=await Users.findOneAndUpdate({_id:finduser._id},{last_login},{new:true});
-    // if(!updateuser){
-    //     next({ status: 404, message: "can not update last login" }); 
-    //     return
-    // }
-    req.session.user = finduser;
-    
-    next();
-  } catch (err) {
-    const error=new Error(err.message)
-        error.status=401
-    next(error);
-  }
+	const { email, password } = req.body;
+	try {
+		const finduser = await Users.findOne({ email });
+		// if (!finduser) {
+		//   next({ status: 404, message: "user not found you need to signup" });
+		//   return;
+		// }
+		if (finduser.password !== password) {
+			const err = new Error("Wrong password !");
+			err.status = 401;
+			next(err);
+			return;
+		}
+		const last_login = new Date();
+		const updateuser = await Users.findOneAndUpdate(
+			{ _id: finduser._id },
+			{ last_login },
+			{ new: true }
+		);
+		// if(!updateuser){
+		//     next({ status: 404, message: "can not update last login" });
+		//     return
+		// }
+		req.session.user = finduser;
+
+		next();
+	} catch (err) {
+		const error = new Error(err.message);
+		error.status = 401;
+		next(error);
+	}
 }
-
-
-
 
 //register controller
 async function SignUpController(req, res, next) {
-    
-  const { email, user_name } = req.body;
-  try {
+	const { email, user_name } = req.body;
+	try {
+		const finduser = await Users.findOne({ $or: [{ email }, { user_name }] });
 
-    const finduser = await Users.findOne({ $or: [{ email }, { user_name }] });
-    
-    if (finduser) {
-        const error=new Error('that it is not possible to create a resource with the given definition because another resource already exists with the same attributes')
-        error.status=401
-     next(error)
-      return;
-    }
+		if (finduser) {
+			const error = new Error(
+				"that it is not possible to create a resource with the given definition because another resource already exists with the same attributes"
+			);
+			error.status = 401;
+			next(error);
+			return;
+		}
 
-    const createUser=await Users.create(req.body);
+		const createUser = (await Users.create({ ...req.body, id: v4() })).save();
 
-    if(!createUser){
-        const error=new Error('can not creat')
-        error.status=401
-     next(error)
-      return;  
-    }
+		if (!createUser) {
+			const error = new Error("can not creat");
+			error.status = 401;
+			next(error);
+			return;
+		}
 
-    res.status(201).json({
-        status:"success",
-        message:"user created successfully "
-    })
-
-   
-  } catch (error) {
-    const err=new Error(error.message)
-        err.status=401
-    next(err)
-  }
+		res.status(201).json({
+			status: "success",
+			message: "user created successfully ",
+		});
+	} catch (error) {
+		const err = new Error(error.message);
+		err.status = 401;
+		next(err);
+	}
 }
 
 //create a jwt token controller and send it with data of user
 function GenerateJWTController(req, res, next) {
-  const token = generateJWT(req.session.user);
-  res.status(201).json({
-    status: "success",
-    access_token: token,
-    data: req.session.user,
-    refrech_token: "",
-  });
+	const token = generateJWT(req.session.user);
+	res.status(201).json({
+		status: "success",
+		access_token: token,
+		data: req.session.user,
+		refrech_token: "",
+	});
 }
 
 //verify jwt token controller
-function VerfyJWT(req,res,next){
-const token=verify()
-
-
+function VerfyJWT(req, res, next) {
+	const token = verify();
 }
-
-
-
-
-
-
 
 //function generate jwt
 function generateJWT(UserData) {
-  const token = sign(JSON.stringify(UserData), process.env.JWT_SECRET,{expiresIn:"1d"});
-  return token;
+	const token = sign(JSON.stringify(UserData), process.env.JWT_SECRET, {
+		expiresIn: "1d",
+	});
+	return token;
 }
 
 module.exports = {
-  CheckSignInController,
-  GenerateJWTController,
-  SignUpController
+	CheckSignInController,
+	GenerateJWTController,
+	SignUpController,
 };
--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,55 @@
-const {Schema,model}=require('mongoose');
-const uuid=require('uuid')
+const { Schema, model } = require("mongoose");
+const { v4 } = require("uuid");
 
-const userschema=new Schema({
-    id:{
-        type:String,
-        unique:true,
-        default: uuid.v4()
-    },
-    first_name:{
-        type:String,
-        required:true
-    },
-    last_name:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true
-    },
-    role:{
-        type:String,
-        required:true,
-    },
-    user_name:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    creation_date:{
-        type:String,
-        default:new Date()
-    },
-    last_login:{
-        type:Number
-    },
-    last_update:{
-        type:Number
-    },
-    active:{
-       type: Boolean,
-       default:true
-    }
+const userschema = new Schema({
+	id: {
+		type: String,
+		unique: true,
+	},
+	first_name: {
+		type: String,
+		required: true,
+	},
+	last_name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+	},
+	role: {
+		type: String,
+		required: true,
+	},
+	user_name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	creation_date: {
+		type: String,
+		default: new Date(),
+	},
+	last_login: {
+		type: Number,
+	},
+	last_update: {
+		type: Number,
+	},
+	active: {
+		type: Boolean,
+		default: true,
+	},
+});
 
-})
-
-const Users=model('user',userschema);
-module.exports={
-    Users
-}
+const Users = model("user", userschema);
+module.exports = {
+	Users,
+};
